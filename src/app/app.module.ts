4518b4164a62e0c4b8f216be7af4faeada471d17
Saurabh Ajmera <sajmera@live.com>
2016-12-20 10:16:06 -0800

    adding components and routerModule to ngSpotify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { AboutComponent } from './about/about.component';
+import {ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path: 'contact', component:ContactComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SearchComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
